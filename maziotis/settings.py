51a4ff778a4db4d15cbac7e6de1e8d7b56a5a894
kostasvit <kostasvit@gmail.com>
2020-03-25 21:25:36 +0200

    AcsServices-CRM

--- maziotis/settings.py
+++ maziotis/settings.py
@@ -91,11 +91,18 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'acsservices',
         'USER': 'kostasvit',
-        'PASSWORD': '@c$2019',
+       'PASSWORD': '@c$2019',
         'HOST': 'localhost',
         'PORT': '',
     }
 }
+
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   # }
+#}
 
 
 # Password validation
