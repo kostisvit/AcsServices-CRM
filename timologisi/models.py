cc5334064513eb520e1f54b415e640156afcb440
kostasvit <kostasvit@gmail.com>
2020-10-01 14:12:55 +0300

    update timologisi models.py

--- timologisi/models.py
+++ timologisi/models.py
@@ -33,9 +33,19 @@ class Symbasi(models.Model):
     end_date = models.DateField(default=datetime.date.today, verbose_name='Ημ.Λήξης', null=False, blank=False)
     sign_date = models.DateField(default=datetime.date.today, verbose_name='Ημ.Υπογρ.', null=False, blank=False)
     symbasi = models.CharField(max_length=100, choices=symbasi_type, null=False, blank=False)
+    epafi = models.ForeignKey('dhmoi.Employee', on_delete=models.CASCADE, verbose_name='Υπ.Επικ.', null=False, blank=False,default='empty')
+    poso = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Ποσό', null=False, blank=False, default=0)
     #symbasi_code = models.PositiveIntegerField(default=0, null=False)
     symbasi_description = models.TextField(max_length=600, null=True, blank=True)
 
+    @property
+    def euro_amount(self):
+        return "€%s" % self.poso if self.poso else ""
+
+    class Meta:
+        verbose_name = 'Σύμβαση'
+        verbose_name_plural = 'Σύμβαση'
+        ordering = ['id']
 
     
     
