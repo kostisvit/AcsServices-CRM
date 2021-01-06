0c76651d6e6a956f9d295ab34c82d75d1c9d6254
kostasvit <kostasvit@gmail.com>
2021-01-06 18:51:23 +0200

    create ergasia&polisi chart

--- dhmoi/charts.py
+++ dhmoi/charts.py
@@ -14,7 +14,7 @@ class AdeiaChartView(TemplateView):
         today = datetime.date.today()
         context = super().get_context_data(**kwargs)
         query = self.request.GET.get('q')
-        context["qs"] = Adeia.objects.values('employee__first_name','employee__last_name','adeiatype').filter(Q(createddate__year=query)).exclude(adeiatype='2').annotate(days=Sum('days'))
+        context["qs"] = Adeia.objects.values('employee__first_name','employee__last_name').filter(Q(createddate__year=query)).exclude(adeiatype='2').annotate(days=Sum('days'))
         return context
 
 
@@ -24,7 +24,8 @@ class PolisiChartView(TemplateView):
     def get_context_data(self, **kwargs):
         today = datetime.date.today()
         context = super().get_context_data(**kwargs)
-        context["qs"] = Polisi.objects.values('dhmos__name').annotate(total=Cast('sinoltimi',output_field=FloatField()))
+        query =self.request.GET.get('q')
+        context["qs"] = Polisi.objects.values('dhmos__name').filter(Q(etos=query)).annotate(total=Sum('sinoltimi'))
         return context
 
 
@@ -34,5 +35,6 @@ class ErgasiaChartView(TemplateView):
     def get_context_data(self, **kwargs):
         today = datetime.date.today()
         context = super().get_context_data(**kwargs)
-        context["qs"] = Ergasies.objects.values('dhmos__name').order_by('dhmos__name').filter(importdate__year=today.year).annotate(total_time=Sum('time'))
+        query = self.request.GET.get('q')
+        context["qs"] = Ergasies.objects.values('dhmos__name').order_by('dhmos__name').filter(Q(importdate__year=query)).annotate(total_time=Sum('time'))
         return context
